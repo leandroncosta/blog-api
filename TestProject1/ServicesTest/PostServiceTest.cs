2bcfa45dc0747a2a80f238f7ac65cd3013629413
Thauan <126874939+Thauan-S@users.noreply.github.com>
2024-12-19 14:39:28 -0300

    feat: criação dos testes unitários do PostService e PostController

---
+++ TestProject1/ServicesTest/PostServiceTest.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using api.Controllers;
+using api.Models;
+using api.Services;
+using api.Services.PostService;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+using Moq;
+
+namespace TestProject1.ServicesTest
+{
+    public class PostServiceTest 
+    {
+
+        private readonly PostService _postService;
+        private readonly Mock<IPostRepository> _postRepositoryMock=new Mock<IPostRepository>();
+        public PostServiceTest()
+        {
+            _postService = new PostService(_postRepositoryMock.Object);
+
+        }
+        private Post Post = new Post()
+        {
+            Id = "1",
+            UserId = "1",
+            Content = "content",
+            Date = DateTime.Now,
+            Title = "title",
+            
+        };
+        private Post Post2 = new Post()
+        {
+            Id = "2",
+            UserId = "2",
+            Content = "content2",
+            Date = DateTime.Now,
+            Title = "title2",
+
+        };
+        List<Post> PostsList = new List<Post>();
+      
+
+        [Fact(DisplayName = "Should return all posts with success when posts exists ")]
+        public async void GetPosts_ShouldReturnAllPosts_WhenPostsExists()
+        {
+            //Arrange
+            PostsList.Add(Post);
+            var expected = PostsList[0];
+
+          
+            _postRepositoryMock.Setup(p => p.GetPosts()).ReturnsAsync(PostsList);
+            //Act 
+            var actual = await _postService.GetPosts();
+            //Assert
+            Assert.NotNull(actual);
+            Assert.Equal(PostsList.Count, actual.Count);
+            Assert.Equal(expected.Id, actual[0].Id);
+            Assert.Equal(expected.UserId, actual[0].UserId);
+            Assert.Equal(expected.Title, actual[0].Title);
+            Assert.Equal(expected.Date, actual[0].Date);
+            Assert.Equal(expected.Content, actual[0].Content);
+            _postRepositoryMock.Verify(p=> p.GetPosts(),Times.Once);
+        }
+        [Fact(DisplayName = "Should return a empty list  when posts not exists")]
+        public async void GetPosts_ShouldReturnAemptyList_WhenPostsNotExists()
+        {
+            //Arrange
+            PostsList.ToList();
+            
+            _postRepositoryMock.Setup(p => p.GetPosts()).ReturnsAsync(PostsList);
+            //Act 
+            var actual = await _postService.GetPosts();
+            await _postService.GetPosts();
+            //Assert
+            Assert.NotNull(actual);
+            Assert.Empty(actual);
+            _postRepositoryMock.Verify(p => p.GetPosts(), Times.Exactly(2));
+
+        }
+
+        [Fact(DisplayName ="Should return a post when is created")]
+        public async void CreatePost_ShouldReturnThePost_WhenPostIsCreated()
+        {
+            //Arrange
+            var userId = Post.UserId;
+            var post = Post;
+        
+            _postRepositoryMock.Setup(p => p.CreatePost(userId,post)).ReturnsAsync(post);
+            //Act 
+            var actual = await _postService.CreatePost(userId,post);
+            //Assert
+            Assert.NotNull(actual);
+            Assert.Equal(Post.Id, actual.Id);
+            Assert.Equal(userId, actual.UserId);
+            Assert.Equal(Post, actual);
+            Assert.Equal(Post.Title, actual.Title);
+            Assert.Equal(Post.Content, actual.Content);
+            Assert.Equal(Post.Date, actual.Date);
+            _postRepositoryMock.Verify((p) => p.CreatePost(userId, post),Times.Once);
+        }
+
+        [Fact(DisplayName = " Should throw an exception when the post is not created")]
+        public async void CreatePost_ShouldThrowAnException_WhenPostIsNotCreated()
+        {
+            //Arrange
+            var userId =Post.UserId;
+            var post = Post;
+            
+            _postRepositoryMock.Setup(p => p.CreatePost(It.IsAny<string>(), It.IsAny<Post>())).ThrowsAsync(new Exception());
+            
+            // Act & Assert
+           
+            var exception =await Assert.ThrowsAsync<Exception>(() =>  _postService.CreatePost(userId, Post));
+            Assert.IsType<Exception>(exception);
+            Assert.NotNull(exception.Message);
+            _postRepositoryMock.Verify(p => p.CreatePost(It.IsAny<string>(),It.IsAny<Post>()), Times.Once);
+
+        }
+
+        [Fact(DisplayName ="Should Return all posts of user With success")]
+        public async void GetPostsByUserId_SloudReturnAllPostsOfUser_WhenPostsExists()
+        {
+            //Arrange
+            PostsList.Add(Post);
+            PostsList.Add(Post2);
+            var userId = Post.UserId;
+            var userId2 = Post2.UserId;
+            _postRepositoryMock.Setup(p => p.GetPostsByUserId(userId)).ReturnsAsync(PostsList);
+            //Act
+            var actual =await  _postService.GetPostsByUserId(userId);
+          
+            //Assert
+            Assert.NotNull(actual);
+            Assert.Equal(PostsList.Count,actual.Count);
+            Assert.Equal(userId, actual[0].UserId);
+            Assert.Equal(userId2, actual[1].UserId);
+            _postRepositoryMock.Verify(p => p.GetPostsByUserId(userId), Times.Once);
+
+        }
+        [Fact(DisplayName = "Should throw an exception when the posts not exists")]
+        public async void GetPostsByUserId_SloudThowsAnException_WhenPostsNotExists()
+        {
+            //Arrange
+            _postRepositoryMock.Setup(p => p.GetPostsByUserId(It.IsAny<string>())).ThrowsAsync(new Exception());
+            //Act & Assert
+            var exception=await Assert.ThrowsAsync<Exception>(()=>_postService.GetPostsByUserId(""));
+            Assert.IsType<Exception>(exception);
+            _postRepositoryMock.Verify(p => p.GetPostsByUserId(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact(DisplayName ="Should update and return a post with success")]
+        public async void UpdatePost_ShouldReturnTheSuccessfullyUpdatedPost_WhenPostExists()
+        {
+            //Arrange
+            var id=Post.Id;
+            var post = Post;
+            _postRepositoryMock.Setup(p => p.Put(id, post)).ReturnsAsync(Post);
+
+            //Act
+            var actual= await _postService.Put(id, post);
+            //Assert
+            Assert.NotNull(actual);
+            Assert.Equal(post.Id, actual.Id);
+            Assert.Equal(post.UserId, actual.UserId);
+            Assert.Equal(post.Title, actual.Title);
+            Assert.Equal(post.Content, actual.Content);
+            Assert.Equal(post.Date, actual.Date);
+            _postRepositoryMock.Verify(p=> p.Put(id,post),Times.Once);
+        }
+        [Fact(DisplayName = "Should throw  an exception when the post not exists")]
+        public async void UpdatePost_ShouldThrowAnException_WhenPostNotExists()
+        {
+            //Arrange
+            var post = Post;
+            _postRepositoryMock.Setup(p => p.Put(It.IsAny<string>(),It.IsAny<Post>())).ThrowsAsync(new Exception());
+            //Act && Assert
+            var exception = await Assert.ThrowsAsync<Exception>(() => _postService.Put("",post));
+            Assert.IsType<Exception>(exception);
+            _postRepositoryMock.Verify(p => p.Put(It.IsAny<string>(),It.IsAny<Post>()), Times.Once);
+        }
+        
+        [Fact(DisplayName ="Should delete a post with success")]
+        public async void Delete_ShouldReturnTrue_WhenPostIsDeletedWithSuccess()
+        {
+            //Arrange
+            var id = Post.Id;
+            _postRepositoryMock.Setup(p =>p.Delete(id)).ReturnsAsync(true);
+            //Act
+            var actual= await _postService.Delete(id);
+            //Assert
+            Assert.True(actual);
+            _postRepositoryMock.Verify(p=>p.Delete(id),Times.Once);
+        }
+
+        [Fact(DisplayName = "Should return false when post found ")]
+        public async void Delete_ShouldReturnFalse_WhenNotFound()
+        {
+            //Arrange
+            var id = Post.Id;
+            _postRepositoryMock.Setup(p => p.Delete(It.IsAny<string>())).ReturnsAsync(false);
+            //Act
+            var actual = await _postService.Delete("");
+            //Assert
+            Assert.False(actual);
+            _postRepositoryMock.Verify(p => p.Delete(""), Times.Once);
+        }
+    }
+}
---
+++ api/Repository/PostRepository.cs
@@ -0,0 +1,105 @@
+﻿
+
+using System.Collections.Generic;
+using api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+
+namespace api.Services.PostService
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly IMongoCollection<Post> _postsCollection;
+        private readonly IMongoCollection<User> _usersCollection;
+        public PostRepository(MongoDbService mongoDbService)
+        {
+            _postsCollection = mongoDbService.GetCollection<Post>("Post");
+            _usersCollection = mongoDbService.GetCollection<User>("User");
+        }
+        public async Task<List<Post>> GetPosts()
+        {
+            List<Post> postsDb;
+            try
+            {
+                return postsDb = await _postsCollection.Find(_ => true).ToListAsync(); ;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Post> GetPostById(string id)
+        {
+            try
+            {
+                var post = await _postsCollection.Find(p => p.Id == id).FirstOrDefaultAsync();
+                return post;
+            }
+            catch (Exception ex) {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+        public async Task<Post> CreatePost(string userId, Post post)
+        {
+            post.UserId = userId;
+            try
+            {
+                await _postsCollection.InsertOneAsync(post);
+                return post;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task<List<Post>> GetPostsByUserId(string userId)
+        {
+            try
+            {
+                return await _postsCollection.Find(p => p.UserId == userId).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task<Post> Put(string id, Post post)
+        {
+            try
+            {
+                var postDb = _postsCollection.Find(p => p.Id.Equals(id)).FirstOrDefault();
+                if (postDb == null)
+                {
+                    return postDb = new Post();
+                }
+                postDb.Title = post.Title;
+                postDb.Content = post.Content;
+                await _postsCollection.ReplaceOneAsync(p => p.Id.Equals(id), postDb);
+                return postDb;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task<bool> Delete(string id)
+        {
+            var postDb = _postsCollection.Find(p => p.Id == id).FirstOrDefaultAsync();
+            var isDeleted = await _postsCollection.DeleteOneAsync(p => p.Id == id);
+            if (isDeleted.IsAcknowledged)
+                return true;
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
+
